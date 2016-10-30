7324b35abb720979a4f7e9da1e5deba0e9fad8b6
Sylvenas <sylvenas@outlook.com>
2016-10-30 22:46:56 +0800

    init repository

---
+++ src/app/main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {render} from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import App from './App'; // Our custom react component
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+// Render the main app react component into the app div.
+// For more details see: https://facebook.github.io/react/docs/top-level-api.html#react.render
+render(<App />, document.getElementById('root'));
+
+/**
+ * In this file, we create a React component
+ * which incorporates components provided by Material-UI.
+ */
+
