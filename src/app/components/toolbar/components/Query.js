b12802be14a4203a41f2a2d557f228ca3d062fe6
sylvenas <sylvenas@outlook.com>
2016-11-29 22:00:40 +0800

    修改Query的checkBox的样式

--- src/app/components/toolbar/components/Query.js
+++ src/app/components/toolbar/components/Query.js
@@ -1,7 +1,10 @@
 import React, { Component, PropTypes } from 'react';
+import Checkbox from 'material-ui/Checkbox';
+import { List, ListItem } from 'material-ui/List';
+import Subheader from 'material-ui/Subheader';
 
 const styles = {
-    panel: {
+    root: {
         position: 'absolute',
         top: '58px',
         right: '0px',
@@ -10,14 +13,62 @@ const styles = {
         padding: '12px',
         borderRadius: '2px',
         boxShadow: 'rgba(0, 0, 0, 0.2) 0px 2px 6px 0px, rgba(0, 0, 0, 0.188235) 0px 2px 6px 0px'
+    },
+    item: {
+        width: '60px',
+        float: 'left',
+        margin: '2px 7px',
+        textAlign: 'center',
+        cursor: 'pointer'
+    },
+    img: {
+        position:'absolute',
+        width: '35px',
+        height: '35px',
+        borderRadius: '50%',
+        overflow: 'hidden',
+        marginTop: -5,
+        marginLeft: -17
+    },
+    checkbox: {
+        marginTop: 10,
+        marginLeft: 20
     }
 }
 
 class Query extends Component {
     render() {
         return (
-            <div style={styles.panel}>
-                <h1>Query</h1>
+            <div style={styles.root}>
+                <div style={styles.item}>
+                    <Checkbox
+                        style={styles.checkbox}
+                        checkedIcon={<img style={styles.img} src="img/meishi.png" />}
+                        uncheckedIcon={<img style={styles.img} src="img/meishi_bw.png" />} />
+                        <p>美食</p>
+                </div>
+                <div style={styles.item}>
+                    <Checkbox
+                        style={styles.checkbox}
+                        checkedIcon={<img style={styles.img} src="img/hotel.png" />}
+                        uncheckedIcon={<img style={styles.img} src="img/hotel_bw.png" />} />
+                        <p>酒店</p>
+                </div>
+                <div style={styles.item}>
+                    <Checkbox
+                        style={styles.checkbox}
+                        checkedIcon={<img style={styles.img} src="img/scene.png" />}
+                        uncheckedIcon={<img style={styles.img} src="img/scene_bw.png" />} />
+                        <p>景点</p>
+                </div>
+                <div style={styles.item}>
+                    <Checkbox
+                        style={styles.checkbox}
+                        checkedIcon={<img style={styles.img} src="img/residence.png" />}
+                        uncheckedIcon={<img style={styles.img} src="img/residence_bw.png" />} />
+                        <p>小区</p>
+                </div>
+                <h1>qq</h1>
             </div>
         );
     }
---
+++ src/app/components/toolbar/containers/QueryContainer.js
@@ -0,0 +1,25 @@
+import { connect } from 'react-redux'
+import Query from '../components/Query'
+import {actionCreator} from 'actionCreatorPath'
+
+const mapStateToProps = (state, ownProps) => {
+    return {
+        prop: state.prop
+    }
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+    return {
+        dispatch1: () =>{
+            dispatch(actionCreator)
+        }
+    }
+}
+
+const mergeProps = (stateProps, dispatchProps, ownProps) => {
+    return {
+        mergeProp: mergePropVal
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps, mergeProps)(Query)
